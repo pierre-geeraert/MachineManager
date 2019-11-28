58f497372ae0684f61639bd2fccc9c74df8e4980
Cloudmanger_user <root@Cloudmanager.geeraert.eu>
2019-11-28 15:03:34 +0000

    add credentials files with fake value to show syntax

---
+++ cloudmachinemanager/credentials.py
@@ -0,0 +1,14 @@
+class proxmox:
+    class server1:
+        url_proxmox = 'domain.com'
+        user='root@pve'
+        password = 'password'
+        node = 'pve'
+
+class sql:
+    url = 'localhost'
+    port = 3306
+    user = 'DB_user'
+    password = 'DB_password'
+    database = 'DB_name'
+
